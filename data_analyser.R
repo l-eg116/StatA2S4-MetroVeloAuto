4fbd8205c32400063afdb6db61d036bc9fb87597
l_eg <legcom6@gmail.com>
2022-04-30 11:25:09 +0200

    Ajout de packages et de distance

--- data_analyser.R
+++ data_analyser.R
@@ -1,4 +1,13 @@
 # Emile & Emma - Copyright 2022
+# Packages
+if (!require("splitstackshape")) install.packages("splitstackshape")
+library(splitstackshape)
+
+# Functions
+distance = function(x1, y1, x2, y2){
+  return(sqrt((x1 - x2)^2 + (y1 - y2)^2))
+}
+
 
 # Initialisation des jeux de données
 freq_stations = read.csv("trafic-annuel-entrant-par-station-du-reseau-ferre-2021.csv", header = TRUE, sep = ";") # Fréquentations des staions
